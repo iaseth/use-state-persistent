afbc26249dc5d2809134d77517447d83aa4cca25
Ankur Seth <iankurseth@gmail.com>
2024-11-22 16:51:10 +0530

    Made the hook work with any type

--- src/useStatePersistent.ts
+++ src/useStatePersistent.ts
@@ -3,9 +3,10 @@ import React from "react";
 
 
 
-export const useStatePersistent = (key: string, intial: string): [string, (v: string) => void] => {
+export const useStatePersistent = <MyType>(key: string, intial: MyType): [MyType, (v: MyType) => void] => {
 	const [value, setValue] = React.useState(getFromLocalStorage(key, intial));
-	const setValueLS = (v: string) => {
+
+	const setValueLS = (v: MyType) => {
 		setValue(v);
 		LS.setItem(key, JSON.stringify(v));
 	};
